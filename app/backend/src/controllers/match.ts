27564880fec549272493a6aa14fb0c0143992143
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-05-18 19:44:25 -0300

    feat: cria endpoint '/matches' que possibilita filtrar as partidas que estão ou não em andamento

--- app/backend/src/controllers/match.ts
+++ app/backend/src/controllers/match.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
 import { matchService } from '../services';
+import { ITeamFilter } from '../commons/interfaces';
 
-const getAll = async (_req: Request, res: Response) => {
-  const matches = await matchService.getAll();
+const getAll = async (req: Request, res: Response) => {
+  const { inProgress } = req.query;
+  const filter = {} as ITeamFilter;
+  if (inProgress !== undefined) {
+    filter.inProgress = inProgress === 'true';
+  }
+  const matches = await matchService.getAll(filter);
   res.status(200).json(matches);
 };
 
--- app/backend/src/services/match.ts
+++ app/backend/src/services/match.ts
@@ -1,7 +1,13 @@
 import Match from '../database/models/Match';
+import { ITeamFilter } from '../commons/interfaces';
 
-const getAll = async () => {
+const getAll = async (filters: ITeamFilter) => {
+  const where = {} as ITeamFilter;
+  if (filters.inProgress !== undefined) {
+    where.inProgress = filters.inProgress;
+  }
   const matches = await Match.findAll({
+    where: { ...where },
     include: [
       { association: 'homeTeam', attributes: ['teamName'] },
       { association: 'awayTeam', attributes: ['teamName'] },
