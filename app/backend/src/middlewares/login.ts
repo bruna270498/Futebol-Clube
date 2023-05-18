13c58ffe4683cd5c5d36d29facd3b6c768e7060c
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-05-18 14:57:19 -0300

    feat: cria validação de formato de e-mail e password

--- app/backend/src/middlewares/login.ts
+++ app/backend/src/middlewares/login.ts
@@ -1,11 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
-import { BadRequest } from '../commons/errors';
+import { BadRequest, Unauthorized } from '../commons/errors';
+
+const inputInvalid = 'Invalid email or password';
+
+const validateEmail = (email: string) => {
+  const regex = /^(?!.*\.\.)[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+};
+
+const validatePassword = (password: string) => {
+  if (password.length < 6) return false;
+  return true;
+};
 
 const validateLogin = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
-  if (!email || !password) {
-    throw new BadRequest('All fields must be filled');
-  }
+  if (!email || !password) throw new BadRequest('All fields must be filled');
+  if (!validateEmail(email)) throw new Unauthorized(inputInvalid);
+  if (!validatePassword(password)) throw new Unauthorized(inputInvalid);
   next();
 };
 
